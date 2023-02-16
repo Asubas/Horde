f0c021015ccde33f208a3fcdc5f854361fc6e16a
Леха <znaxor74@mail.ru>
2023-02-16 20:36:18 +0300

    делаем красиво

--- js/pets.js
+++ js/pets.js
@@ -1,15 +1,35 @@
 const pets = [
     {
       dataId: 'lion',
-      src: 'img/pets/lion.png',
+      src: 'img/pets/lion_guide.png',
     },
     {
-      dataId: 'snow',
-      src: 'img/zolik.png',
+      dataId: 'fire_dragon',
+      src: 'img/pets/fire_dragon_guide.png',
     },
     {
-      dataId: 'dragon',
-      src: 'img/estra.png',
+      dataId: 'shield',
+      src: 'img/pets/shield_beast_guide.png',
+    },
+    {
+      dataId: 'fire_beast',
+      src: 'img/pets/fire_beast_shield_guide.png',
+    },
+    {
+      dataId: 'yasher',
+      src: 'img/pets/stone_guide.png',
+    },
+    {
+      dataId: 'ice',
+      src: 'img/pets/ice_beast_guide.png',
+    },
+    {
+      dataId: 'owl',
+      src: 'img/pets/owl_guide.png',
+    },
+    {
+      dataId: 'night_cat',
+      src: 'img/pets/night_cat_guide.png',
     },
 ];
 
@@ -27,3 +47,22 @@ pets_show.addEventListener("click", function () {
     pets_back.classList.remove("modal_show");
     pets_back.classList.add("visually_hidden");
   })
+
+
+  const containerButtonsPets = document.querySelectorAll('.button_pet');
+ 
+  function showPictures(src = '') {
+    
+    let img = modal?.querySelector('img');
+    img.src = src;
+    modal?.classList.remove('visually_hidden');
+    modal?.classList.add('modal_show');
+  }
+
+  containerButtonsPets.forEach(elem => {
+    const elemDataId = elem.getAttribute('data-id') || null; // вытаскиваещь значение дата атрибута
+    const imgSrc =
+      pets.find(({ dataId }) => dataId === elemDataId)?.src || // проходишься по массиву с данными , в нем ищещь картинку у которой dataId  как у кнопки data-id
+      '/ссылка на ТАКОЙ КАРТИНКИ НЕ НАЙДЕНО'; // добавляешь ссылку на заглушуку если такой картинки у тебя в массиве нет
+    elem.addEventListener('click', () => showPictures(imgSrc)); // добавляешь эвент на кнопку в котором прокидываешь в функцию параметр src для картинки
+  });
