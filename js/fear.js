a677f660c25341cdee2888457021a33a3b57ec3e
Леха <znaxor74@mail.ru>
2023-03-31 22:02:52 +0300

    добавил кошмары

---
+++ js/fear.js
@@ -0,0 +1,15 @@
+
+const fear = document.querySelectorAll('.fear');
+
+
+pm_visually[10].addEventListener('click', function(){
+    for (let i = 0; i < fear.length; i++){
+    if (fear[i].classList.contains('fear-visually') ){
+        fear[i].classList.remove('fear-visually');
+
+    }else{
+        fear[i].classList.add('fear-visually')
+    }
+}
+})
+
