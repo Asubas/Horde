9bf72048619da721ade37940996096ff4a105a10
Леха <znaxor74@mail.ru>
2023-06-06 19:41:03 +0300

    ничо нового

---
+++ js/multiclass.js
@@ -0,0 +1,4 @@
+let element = document.querySelector('.pm');
+let classList = element.classList; // получаем список классов элемента
+let classCount = classList.length; // считаем количество классов
+console.log(`Количество классов: ${classCount}`);
